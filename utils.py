d6574a6930f88d46a7ede0e0e79023db6224c2b5
ZangZehua <zhzang97@gmail.com>
2022-04-27 13:58:29 +0800

    rollback [yaml] in [utils.py]

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-# import os
+import os
 import shutil
 
 import torch
-# import yaml
+import yaml
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
@@ -11,11 +11,11 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
         shutil.copyfile(filename, 'model_best.pth.tar')
 
 
-# def save_config_file(model_checkpoints_folder, args):
-#     if not os.path.exists(model_checkpoints_folder):
-#         os.makedirs(model_checkpoints_folder)
-#         with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
-#             yaml.dump(args, outfile, default_flow_style=False)
+def save_config_file(model_checkpoints_folder, args):
+    if not os.path.exists(model_checkpoints_folder):
+        os.makedirs(model_checkpoints_folder)
+        with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
+            yaml.dump(args, outfile, default_flow_style=False)
 
 
 def accuracy(output, target, topk=(1,)):
